27773d5a15e7c162c91b719bb04660f4c8f80302
Alferious <okongoalferious@gmail.com>
2024-09-14 13:01:18 +0300

    Youtube downloader

---
+++ youtube.py
@@ -0,0 +1,26 @@
+# Create a folder, in my case, Capstone project
+
+# Create a virtual environment
+
+#Install yt-dlp package and the ffmpeg package
+
+#The yt-dip package is a command line program that lets you easily download videos and audios.
+
+#FFMPEG package is a free and open source software that offers many tools for video and audio processing
+
+#pip install yt-dlp and pip install ffmpeg
+#You can confirm the version used to make sure it was really installed
+
+        #HOW TO DOWNLOAD VIDEOS
+
+# 1. To download You Tube videos;
+#yt-dlp "paste the URL"
+
+# 2. To download You Tube video as mp3;
+#yt-dlp -x --audio-format mp3 "URL"
+
+# 3. To download You Tube playlists
+#yt-dlp -i "url of the playlist"
+
+
+
